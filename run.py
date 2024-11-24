26d97a7ff99a1ca9bbabc2c2db31eb782ec4faab
Ashley Goward <ashleygoward@icloud.com>
2024-11-24 09:00:22 +0000

    Finalise setting up flask

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import os
 from flask import Flask
 
-app = Flask(**name**)
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-return "Hello, World"
+    return "Hello, World"
 
-if **name** == "**main**":
-app.run(
-host=os.environ.get("IP", "0.0.0.0"),
-port=int(os.environ.get("PORT", "5000")),
-debug=True)
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
